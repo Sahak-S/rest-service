39df6e9af9d46cd5bac554726d4f5f4f06c3bedf
Sahak-S <sahak.sa@mail.ru>
2023-02-27 23:27:35 +0400

    rest service test

---
+++ src/main/java/com/example/restservice/ErrorPresentation.java
@@ -0,0 +1,8 @@
+package com.example.restservice;
+
+import java.util.List;
+
+public record ErrorPresentation(List<String> errors) {
+
+
+}
---
+++ src/main/java/com/example/restservice/TaskRepository.java
@@ -0,0 +1,17 @@
+package com.example.restservice;
+
+import org.springframework.http.ProblemDetail;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+public interface TaskRepository {
+    List<Task>findAll();
+
+    void save(Task task);
+
+    Optional<Task> findById(UUID id);
+}
